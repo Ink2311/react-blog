2929e15c81c7fa0bab26947d7e60a0356a40195b
Ink <dombrovskiy.nikitos@gmail.com>
2025-05-21 19:39:30 +0500

    Fix empty object pattern in AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import  { useState } from "react";
 import Home from "../components/Home";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Blog from "../components/Blog";
@@ -6,18 +6,29 @@ import Auth from "../components/Auth";
 import ProtectedRoute from "./ProtectedRoute";
 import { auth } from "../firebaseConfig";
 
-const AppRouter = ({
-}) => {
+const AppRouter = () => {
   const [user, setUser] = useState(null);
 
   return (
     <Router>
       <nav>
-        <Link to="/">Главная</Link> | <Link to="/posts">Блог</Link> |  {!user ? (<Link to="/login">Войти</Link>): (<button onClick={() => auth.signOut().then(() => setUser(null))}>Выйти</button>)}
+        <Link to="/">Главная</Link> | <Link to="/posts">Блог</Link> |{" "}
+        {!user ? (
+          <Link to="/login">Войти</Link>
+        ) : (
+          <button onClick={() => auth.signOut().then(() => setUser(null))}>
+            Выйти
+          </button>
+        )}
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<ProtectedRoute user={user} element={<Blog user={user} />} />} />
+        <Route
+          path="/posts"
+          element={
+            <ProtectedRoute user={user} element={<Blog user={user} />} />
+          }
+        />
         <Route path="/login" element={<Auth setUser={setUser} />} />
       </Routes>
     </Router>
